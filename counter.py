1cd9ac235cc5c50b8a98060834ecdbbae2975901
Gabbe200 <gabriel.forsberg@elev.ga.ntig.se>
2025-08-27 10:25:14 +0200

    läktion 1 uppdateringar

--- counter.py
+++ counter.py
@@ -1,9 +1,10 @@
 from collections import Counter
+import os
+
+os.system('cls')
 
 most_common_numbers = Counter([1, 5, 6, 5, 3, 1, 2, 5]).most_common(2)
 print(most_common_numbers)
 # -> [(5, 3), (1, 2)]
 
-most_common_letters = Counter("abcbadfbcb").most_common(3)
-print(most_common_letters)
-# -> [('b', 4), ('a', 2), ('c', 2)]
+print("mest vanlig", most_common[0])
